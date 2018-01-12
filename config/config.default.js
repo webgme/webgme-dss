f391550f8beeebd5361562ace2ab0046826746c0
pmeijer <patrik85@isis.vanderbilt.edu>
2018-01-12 16:33:37 -0600

    Use webgme-cli and add router for handling paths

--- config/config.default.js
+++ config/config.default.js
@@ -2,8 +2,9 @@
 /**
  * @author pmeijer / https://github.com/pmeijer
  */
-let config = require('webgme-engine/config/config.default');
+let config = require('./config.webgme');
 
 config.client.appDir = './build';
+config.mongo.uri = 'mongodb://127.0.0.1:27017/multi';
 
 module.exports = config;

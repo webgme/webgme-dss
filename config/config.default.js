bde9e465431fc0f0c5a9cf5ddc8421f7eea8cdc7
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-01-19 14:18:05 -0600

    Add example icons from MSL

--- config/config.default.js
+++ config/config.default.js
@@ -2,10 +2,15 @@
 /**
  * @author pmeijer / https://github.com/pmeijer
  */
-let config = require('./config.webgme');
+let config = require('./config.webgme'),
+    path = require('path');
 
 config.client.appDir = './build';
 config.mongo.uri = 'mongodb://127.0.0.1:27017/multi';
 config.seedProjects.defaultProject = 'Modelica';
+
+// Listing:     /assets/decoratorSVGList.json
+// Example:     /assets/DecoratorSVG/Modelica.Electrical.Analog.Basic.Ground.svg
+config.visualization.svgDirs = [path.join(__dirname, '..', 'msl_icons')];
 
 module.exports = config;

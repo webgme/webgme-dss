8fd2ed9f3f59e7b7153eab5afe1e1393c9a0eef5
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-02-01 13:43:55 -0600

    Plotter plots the given variables

--- src/client/CenterPanel/Plotter.jsx
+++ src/client/CenterPanel/Plotter.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import SIM_RES from '../LeftPanel/SIM_RES.json';
+import colorHash from '../gme/utils/colorHash';
 
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
@@ -23,23 +24,36 @@ const mapDispatchToProps = dispatch => {
 class Plotter extends Component {
     render() {
         const data = [];
-        console.log('Plotter got variables', this.props.variables);
+        const {variables} = this.props;
+
+        SIM_RES.timeSeries.time.forEach((time, idx) => {
+            let plotPoints = {
+                time: time
+            };
+
+            variables.forEach(varName => {
+                plotPoints[varName] = SIM_RES.timeSeries[varName][idx]
+            });
+
+            data.push(plotPoints);
+        });
+
         return (
             <div>
                 <LineChart width={900} height={600} data={data}
-                           margin={{top: 100, right: 30, left: 150, bottom: 5}}>
-                    <XAxis dataKey="time"/>
-                    <YAxis/>
-                    <CartesianGrid strokeDasharray="3 3"/>
-                    <Tooltip/>
-                    <Legend />
-                    {data[0] ?
-                        Object.keys(data[0])
-                            .filter(varName => varName !== 'time')
-                            .map(varName => {
-                                return <Line key={varName} type="monotone" dataKey={varName} stroke="#82ca9d" />
-                            })
-                         : null}
+                margin={{top: 100, right: 30, left: 150, bottom: 5}}>
+                <XAxis dataKey="time"/>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+                <Legend />
+                {data[0] ?
+                    Object.keys(data[0])
+                        .filter(varName => varName !== 'time')
+                        .map(varName => {
+                            return <Line key={varName} type="monotone" dataKey={varName} stroke={colorHash(varName).rgb}/>
+                        })
+                    : null}
                 </LineChart>
             </div>
         );
---
+++ src/client/gme/utils/colorHash.jsx
@@ -0,0 +1,53 @@
+/*
+	Color Hash
+	By Roland Rytz
+	Generates a pseudo-random color from an input string.
+	This is free and unencumbered software released into the public domain.
+	Anyone is free to copy, modify, publish, use, compile, sell, or
+	distribute this software, either in source code form or as a compiled
+	binary, for any purpose, commercial or non-commercial, and by any
+	means.
+	In jurisdictions that recognize copyright laws, the author or authors
+	of this software dedicate any and all copyright interest in the
+	software to the public domain. We make this dedication for the benefit
+	of the public at large and to the detriment of our heirs and
+	successors. We intend this dedication to be an overt act of
+	relinquishment in perpetuity of all present and future rights to this
+	software under copyright law.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+export default function colorHash(inputString) {
+    let sum = 0;
+
+    for (let i in inputString) {
+        sum += inputString.charCodeAt(i);
+    }
+
+    let r = ~~(('0.' + Math.sin(sum + 1).toString().substr(6)) * 256);
+    let g = ~~(('0.' + Math.sin(sum + 2).toString().substr(6)) * 256);
+    let b = ~~(('0.' + Math.sin(sum + 3).toString().substr(6)) * 256);
+
+    let rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+
+    let hex = "#";
+
+    hex += ("00" + r.toString(16)).substr(-2, 2).toUpperCase();
+    hex += ("00" + g.toString(18)).substr(-2, 2).toUpperCase();
+    hex += ("00" + b.toString(20)).substr(-2, 2).toUpperCase();
+
+    return {
+        r: r
+        , g: g
+        , b: b
+        , rgb: rgb
+        , hex: hex
+    };
+}

ce5c1280453bb488f0eb86562a3933fedc187a94
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-03-29 17:17:48 -0500

    Ensure mongodb is running at server startup

--- server.js
+++ server.js
@@ -5,11 +5,32 @@
 
 let gmeConfig = require('./config'),
     webgme = require('webgme-engine'),
-    myServer;
+    mongodb = require('mongodb');
 
-webgme.addToRequireJsPaths(gmeConfig);
+const CONNECTION_ATTEMPT_TIMEOUT = 2000;
+let CONNECTION_ATTEMPTS = 10;
 
-myServer = new webgme.standaloneServer(gmeConfig);
-myServer.start(function () {
-    //console.log('server up');
-});
+const ensureMongdbRunning = () => {
+    mongodb.MongoClient.connect(gmeConfig.mongo.uri, (err, client) => {
+        if (err) {
+            console.log(`${gmeConfig.mongo.uri} not available, will try ${CONNECTION_ATTEMPTS} more times...`);
+            CONNECTION_ATTEMPTS -= 1;
+            if (CONNECTION_ATTEMPTS <= 0) {
+                console.error(`Failed to connect to ${gmeConfig.mongo.uri}`);
+                process.exit(42);
+            } else {
+                setTimeout(ensureMongdbRunning, CONNECTION_ATTEMPT_TIMEOUT);
+            }
+        } else {
+            client.close(() => {
+                webgme.addToRequireJsPaths(gmeConfig);
+                const myServer = new webgme.standaloneServer(gmeConfig);
+                myServer.start(function () {
+                    //console.log('server up');
+                });
+            });
+        }
+    });
+};
+
+ensureMongdbRunning();

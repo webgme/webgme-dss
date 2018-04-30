1dcfb9e4a687d0327548d0ee84a387363fb12081
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-04-30 12:31:15 -0500

    Send logout request

--- src/client/components/HeaderPanel/User.jsx
+++ src/client/components/HeaderPanel/User.jsx
@@ -46,7 +46,7 @@ export default class User extends Component {
 =; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
         window.parent.postMessage('logout', '*');
 
-        //tempAnchor.click();
+        tempAnchor.click();
     };
 
     render() {
